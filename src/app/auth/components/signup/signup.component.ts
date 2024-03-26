153359910b036a3de63e86156c68f1d8272af947
gurunganish5 <gurunganish5@gmail.com>
2024-03-26 01:34:40 -0400

    signup api and form validation

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,5 +10,34 @@ import { Component } from '@angular/core';
 export class SignupComponent {
 
   isSpinning: boolean = false;
+  signupForm!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService ){}
+
+  ngOnInit(){
+    this.signupForm = this.fb.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      checkPassword: [null, [Validators.required, this.confirmationValidate]],
+    })
+  }
+
+  confirmationValidate = (control: FormControl): { [s:string]: boolean} =>{
+    if(!control.value){
+      return {required: true};
+    }else if(control.value !== this.signupForm.controls['password'].value){
+      return { confirm:true, error:true};
+    }
+    return {};
+  };
+
+  register(){
+    console.log(this.signupForm.value);
+    this.authService.register(this.signupForm.value).subscribe((res) =>{
+      console.log(res);
+    })
+  }
 
 }
