b6cac3ed367ca6514be2a11ae2068ba7ea35b2c5
gurunganish5 <gurunganish5@gmail.com>
2024-03-26 00:34:20 -0400

    angular setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//NG ZORRO IMPORTS
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NzSpinModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    NzLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
